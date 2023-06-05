5acc068be1376653fa0482a08c39911021d05ef1
dreamy <dreamy.zhao@yphrs.com>
2023-06-05 23:42:58 +0800

    QuoteBoard

---
+++ widgets/QuoteBoard/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  <md-protocol />
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import mdProtocol from "./components/mdProtocol.vue"
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    mdProtocol
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: black;
+  color: white;
+  height: 100vh;
+}
+</style>
---
+++ widgets/QuoteBoard/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'xe-utils'
+import VXETable from 'vxe-table'
+import 'vxe-table/lib/style.css'
+createApp(App).use(VXETable).mount('#app')
