e399158f5bbcfbe5e282678fefd7c17d2164b049
krenx1983 <99114024@qq.com>
2023-07-30 16:07:51 +0800

    增加-t选项以支持指定用交易api连接交易前置

--- demo/ctpping/ctpping.cpp
+++ demo/ctpping/ctpping.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#ifdef _WIN32
+#include "getopt.h"
+#else
+#include <unistd.h>
+#endif
 #include "ThostFtdcMdApi.h"
+#include "ThostFtdcTraderApi.h"
 
 auto reqtime = std::chrono::steady_clock::now();
 
 class CMarketSpi :public CThostFtdcMdSpi
 {
 public:
-	CMarketSpi(CThostFtdcMdApi* pApi):m_pMarketApi(pApi)
+	CMarketSpi(CThostFtdcMdApi* pApi):m_pApi(pApi)
 	{
 		pApi->RegisterSpi(this);
 	}
@@ -19,7 +25,7 @@ public:
 
 		reqtime = std::chrono::steady_clock::now();
 		CThostFtdcReqUserLoginField Req = {0};
-		m_pMarketApi->ReqUserLogin(&Req, 0);
+		m_pApi->ReqUserLogin(&Req, 0);
 	}
 
 	void OnFrontDisconnected(int nReason)
@@ -37,24 +43,96 @@ public:
 		exit(0);
 	}
 
-	CThostFtdcMdApi* m_pMarketApi;
+	CThostFtdcMdApi* m_pApi;
 };
 
+class CTradeSpi :public CThostFtdcTraderSpi
+{
+public:
+	CTradeSpi(CThostFtdcTraderApi* pApi) :m_pApi(pApi)
+	{
+		pApi->RegisterSpi(this);
+	}
+
+	void OnFrontConnected()
+	{
+		std::cout << "connected." << std::endl;
+
+		reqtime = std::chrono::steady_clock::now();
+		CThostFtdcReqUserLoginField Req = { 0 };
+		m_pApi->ReqUserLogin(&Req, 0);
+	}
+
+	void OnFrontDisconnected(int nReason)
+	{
+		std::cout << "disconnected." << std::endl;
+		exit(0);
+	}
+
+	void OnRspUserLogin(CThostFtdcRspUserLoginField* pRspUserLogin, CThostFtdcRspInfoField* pRspInfo, int nRequestID, bool bIsLast)
+	{
+		auto rsptime = std::chrono::steady_clock::now();
+		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(rsptime - reqtime);
+
+		std::cout << "response time: " << duration.count() << " milliseconds" << std::endl;
+		exit(0);
+	}
+
+	CThostFtdcTraderApi* m_pApi;
+};
+
+void print_usage()
+{
+	std::cout << "usage:ctpping [-t] [-m] address" << std::endl;
+	std::cout << "example:ctpping tcp://180.168.146.187:10130" << std::endl;
+	std::cout << "example:ctpping -m tcp://180.168.146.187:10131" << std::endl;
+	std::cout << "example:ctpping -t tcp://180.168.146.187:10130" << std::endl;
+}
 int main(int argc,char *argv[])
 {
-	if (argc != 2) {
-		std::cout << "usage:ctpping address" << std::endl;
-		std::cout << "example:ctpping tcp://180.168.146.187:10100" << std::endl;
-		return 0;
+	bool use_trade = false;
+	int ch;
+	while ((ch = getopt(argc, argv, "tm")) != -1)
+	{
+		switch (ch) {
+		case 't':
+			use_trade = true;
+			break;
+		case 'm':
+			break;
+		case '?':
+			print_usage();
+			return -1;
+		default:
+			print_usage();
+			return -1;
+		}
 	}
-	std::cout << "version:" << CThostFtdcMdApi::GetApiVersion() << std::endl;
 
-	CThostFtdcMdApi* pApi = CThostFtdcMdApi::CreateFtdcMdApi();
-	CMarketSpi Spi(pApi);
-	pApi->RegisterFront(argv[1]);
-	pApi->Init();
+	if (optind >= argc) {
+		print_usage();
+		return -1;
+	}
 
-	std::this_thread::sleep_for(std::chrono::seconds(5));
+	if (use_trade) {
+		std::cout << "version:" << CThostFtdcTraderApi::GetApiVersion() << std::endl;
+
+		CThostFtdcTraderApi* pApi = CThostFtdcTraderApi::CreateFtdcTraderApi();
+		CTradeSpi Spi(pApi);
+		pApi->RegisterFront(argv[optind]);
+		pApi->Init();
+		std::this_thread::sleep_for(std::chrono::seconds(5));
+	}
+	else {
+		std::cout << "version:" << CThostFtdcMdApi::GetApiVersion() << std::endl;
+
+		CThostFtdcMdApi* pApi = CThostFtdcMdApi::CreateFtdcMdApi();
+		CMarketSpi Spi(pApi);
+		pApi->RegisterFront(argv[optind]);
+		pApi->Init();
+		std::this_thread::sleep_for(std::chrono::seconds(5));
+	}
+
 	std::cout << "time out." << std::endl;
 
 	return 0;
