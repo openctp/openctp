08a6a868ed7b5432e0314a083c1f9234f35d2427
Jedore <jedore@protonmail.com>
2023-09-06 16:35:23 +0800

    Optimize export_rate.py

--- tools/PythonScripts/export_rate.py
+++ tools/PythonScripts/export_rate.py
@@ -1,49 +1,26 @@
 """
     CTP交易费导出工具
-        查询期货合约的手续费、保证金，以行情中最新价格计算出交易1手时的手续费、保证金, 并生成 csv 文件.
-        具体表头有: "交易所,合约,合约名称,品种,合约乘数,开仓费率,开仓费/手,平仓费率,平仓费/手,平今仓费率,平今仓费/手,最新价,成交量,空盘量,1手开仓手续费,1手平仓手续费,1手平今仓手续费,做多保证金率,做多保证金/手,做空保证金率,做空保证金/手,做多1手保证金,做空1手保证金"
+        查询期货合约的手续费、保证金，以行情中的最新价格计算出交易1手时的手续费、保证金, 并生成 csv 文件
+        具体表头有 "交易所,合约,合约名称,品种,合约乘数,开仓费率,开仓费/手,平仓费率,平仓费/手,平今仓费率,平今仓费/手,最新价,成交量,空盘量,1手开仓手续费,1手平仓手续费,1手平今仓手续费,做多保证金率,做多保证金/手,做空保证金率,做空保证金/手,做多1手保证金,做空1手保证金,最小跳动价位,1Tick盈亏"
 
     支持CTPAPI接口的柜台均可导出
 
     需先安装 openctp-ctp: pip install openctp-ctp
-
-    运行结果如下：
-        python .\export_rate.py
-        -------------------------- 启动CTP交易费导出工具
-        CTPAPI: v6.7.0_20230209  9:52:16.3535
-        交易前置: tcp://180.168.146.187:10130
-        Connect to 180.168.146.187:10130
-        Session -1313406975 Connected
-        交易后台版本: v6.7.1_20230713 16:55:42.4819.tkernel
-        交易日: 20230901
-        投资者: 209025
-        -------------------------- 开始导出CTP交易费
-        1. 查询合约
-                合约数量: 731
-        2. 查询行情
-                行情数量: 731
-        3. 查询交易费率
-                手续费数量: 76
-                保证金数量: 725
-        4. 计算交易费并导出
-                有效合约交易费: 725
-                无效合约交易费: 6
-        -------------------------- 导出CTP交易费完成: 169 秒
 """
 
 import time
-from datetime import datetime as dt
 from pathlib import Path
 from queue import Queue
 
 from openctp_ctp import tdapi
 
-# 使用前一定配置好以下信息
+# 以下信息默认为 simnow 环境，需要按照各自柜台进行配置。
+
 # 用户/密码
 user = ""
 password = ""
 # 交易前置等
-td_front = "tcp://180.168.146.187:10130"  # simnow 7x24
+td_front = "tcp://180.168.146.187:10130"
 broker_id = "9999"
 authcode = "0000000000000000"
 appid = "simnow_client_test"
@@ -158,6 +135,7 @@ class CTdSpi(tdapi.CThostFtdcTraderSpi):
                         "InstrumentName": pInstrument.InstrumentName,
                         "ProductID": pInstrument.ProductID,
                         "VolumeMultiple": pInstrument.VolumeMultiple,
+                        "PriceTick": pInstrument.PriceTick,
                     }
                 )
 
@@ -199,7 +177,8 @@ class CTdSpi(tdapi.CThostFtdcTraderSpi):
         pInstrumentMarginRate: tdapi.CThostFtdcInstrumentMarginRateField,
         pRspInfo: tdapi.CThostFtdcRspInfoField,
         nRequestID: int,
-        bIsLast: bool):
+        bIsLast: bool,
+    ):
         if pRspInfo and pRspInfo.ErrorID:
             print(
                 f"\t查询合约保证金失败: ErrorID={pRspInfo.ErrorID}, ErrorMsg={pRspInfo.ErrorMsg}"
@@ -207,14 +186,16 @@ class CTdSpi(tdapi.CThostFtdcTraderSpi):
             return
 
         if pInstrumentMarginRate:
-            Q_MARGIN.put_nowait({
-                "InstrumentID": pInstrumentMarginRate.InstrumentID,
-                "ExchangeID": pInstrumentMarginRate.ExchangeID,
-                "LongMarginRatioByMoney": pInstrumentMarginRate.LongMarginRatioByMoney,
-                "LongMarginRatioByVolume": pInstrumentMarginRate.LongMarginRatioByVolume,
-                "ShortMarginRatioByMoney": pInstrumentMarginRate.ShortMarginRatioByMoney,
-                "ShortMarginRatioByVolume": pInstrumentMarginRate.ShortMarginRatioByVolume,
-            })
+            Q_MARGIN.put_nowait(
+                {
+                    "InstrumentID": pInstrumentMarginRate.InstrumentID,
+                    "ExchangeID": pInstrumentMarginRate.ExchangeID,
+                    "LongMarginRatioByMoney": pInstrumentMarginRate.LongMarginRatioByMoney,
+                    "LongMarginRatioByVolume": pInstrumentMarginRate.LongMarginRatioByVolume,
+                    "ShortMarginRatioByMoney": pInstrumentMarginRate.ShortMarginRatioByMoney,
+                    "ShortMarginRatioByVolume": pInstrumentMarginRate.ShortMarginRatioByVolume,
+                }
+            )
         else:
             Q_MARGIN.put_nowait(None)
 
@@ -327,8 +308,7 @@ class Export(object):
                 while True:
                     ret = self._spi.api.ReqQryInstrumentCommissionRate(req, 0)
                     if ret == 0:
-                        # 由于不同柜台的请求频率限制不同，按需自定义请求间的睡眠时间
-                        time.sleep(0.5)
+                        time.sleep(0.2)
                         break
                     time.sleep(0.1)
                     print("sleep 0.1")
@@ -346,8 +326,7 @@ class Export(object):
             while True:
                 ret = self._spi.api.ReqQryInstrumentMarginRate(req, 0)
                 if ret == 0:
-                    # 由于不同柜台的请求频率限制不同，按需自定义请求间的睡眠时间
-                    time.sleep(0.5)
+                    time.sleep(0.2)
                     break
                 time.sleep(0.1)
                 print("sleep 0.1")
@@ -360,13 +339,21 @@ class Export(object):
 
     def save(self):
         print("4. 计算交易费并导出")
-        header = "交易所,合约,合约名称,品种,合约乘数,开仓费率,开仓费/手,平仓费率,平仓费/手,平今仓费率,平今仓费/手,最新价,成交量,空盘量,1手开仓手续费,1手平仓手续费,1手平今仓手续费,做多保证金率,做多保证金/手,做空保证金率,做空保证金/手,做多1手保证金,做空1手保证金\n"
+        header = "交易所,合约,合约名称,品种,合约乘数,开仓费率,开仓费/手,平仓费率,平仓费/手,平今仓费率,平今仓费/手,最新价,成交量,空盘量,1手开仓手续费,1手平仓手续费,1手平今仓手续费,做多保证金率,做多保证金/手,做空保证金率,做空保证金/手,做多1手保证金,做空1手保证金,最小跳动价位,1Tick盈亏\n"
         valid_lines, invalid_lines = [header], [header]
         for ins_id, ins in self._d_instrument.items():
             multiple = ins["VolumeMultiple"]
-            line = [ins["ExchangeID"], ins_id, ins["InstrumentName"], ins["ProductID"], str(multiple)]
+            line = [
+                ins["ExchangeID"],
+                ins_id,
+                ins["InstrumentName"],
+                ins["ProductID"],
+                str(multiple),
+            ]
             market = self._d_market_data.get(ins_id, {})
-            rate = self._d_rate.get(ins_id, {}) or self._d_rate.get(ins["ProductID"], {})
+            rate = self._d_rate.get(ins_id, {}) or self._d_rate.get(
+                ins["ProductID"], {}
+            )
             margin = self._d_margin.get(ins["InstrumentID"])
 
             if not market or not rate or not margin:
@@ -384,35 +371,38 @@ class Export(object):
             long_volume = margin.get("LongMarginRatioByVolume")
             short_money = margin.get("ShortMarginRatioByMoney")
             short_volume = margin.get("ShortMarginRatioByVolume")
-            line.extend([
-                f'{open_money:f}',
-                str(open_volume),
-                f'{close_money:f}',
-                str(close_volume),
-                f'{close_today_money:f}',
-                str(close_today_volume),
-                str(last_price),
-                str(market["Volume"]),
-                str(market["OpenInterest"]),
-                str(last_price * multiple * open_money + open_volume),
-                str(last_price * multiple * close_money + close_volume),
-                str(last_price * multiple * close_today_money + close_today_volume),
-                str(long_money),
-                str(long_volume),
-                str(short_money),
-                str(short_volume),
-                str(last_price * multiple * long_money + long_volume),
-                str(last_price * multiple * short_money + short_volume),
-            ])
+            line.extend(
+                [
+                    f"{open_money:f}",
+                    str(open_volume),
+                    f"{close_money:f}",
+                    str(close_volume),
+                    f"{close_today_money:f}",
+                    str(close_today_volume),
+                    str(last_price),
+                    str(market["Volume"]),
+                    str(market["OpenInterest"]),
+                    str(last_price * multiple * open_money + open_volume),
+                    str(last_price * multiple * close_money + close_volume),
+                    str(last_price * multiple * close_today_money + close_today_volume),
+                    str(long_money),
+                    str(long_volume),
+                    str(short_money),
+                    str(short_volume),
+                    str(last_price * multiple * long_money + long_volume),
+                    str(last_price * multiple * short_money + short_volume),
+                    str(ins["PriceTick"]),
+                    str(round(ins["PriceTick"] * multiple, 2)),
+                ]
+            )
 
             valid_lines.append(",".join(line) + "\n")
 
         print("\t有效合约交易费:", len(valid_lines) - 1)
         print("\t无效合约交易费:", len(invalid_lines) - 1)
 
-        today = dt.today().date()
-        valid_name = f"{user}-CTP交易费-{today}.csv"
-        invalid_name = f"{user}-CTP交易费-{today}-无效.csv"
+        valid_name = f"openctp期货交易费用参照表（交易所+1分）-{self._spi.trading_day}.csv"
+        invalid_name = f"openctp期货交易费用参照表（交易所+1分）-{self._spi.trading_day}-无效.csv"
 
         with open(valid_name, mode="w", encoding="utf8") as fp:
             fp.writelines(valid_lines)
