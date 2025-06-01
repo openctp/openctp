ea53cc3e9f9720bbce58c3c2351467c265e7d469
krenx <83346523+krenx1983@users.noreply.github.com>
2025-06-02 02:54:20 +0800

    Delete widgets/QuoteBoard directory

--- widgets/QuoteBoard/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'xe-utils'
-import VXETable from 'vxe-table'
-import 'vxe-table/lib/style.css'
-createApp(App).use(VXETable).mount('#app')
